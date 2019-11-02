a2d61e92368b7d1b5868c41a80775fa219e0b978
Caetano Siemann <caetanosiemann@gmail.com>
2019-11-01 21:33:33 -0300

    Start project

---
+++ src/Cliente.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Cliente {
+	
+	
+	public String nome;
+	public ArrayList<Doce> docesComprados = new ArrayList();
+	
+	public Cliente(String nome) throws Exception{
+		this.setNome(nome);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) throws Exception {
+		if (nome.isEmpty()) {
+			throw new Exception("Nome inválido");
+		}
+		this.nome = nome;
+	}
+
+	public ArrayList<Doce> getDocesComprados() {
+		return docesComprados;
+	}
+
+	public void addDoce(Doce doce) {
+		docesComprados.add(doce);
+	}
+	
+	public float totalCompra() {
+		
+		return 0;
+	}
+		
+}
---
+++ src/Doce.java
@@ -0,0 +1,45 @@
+public class Doce {
+
+	public int codigo;
+	public String nome;
+	public float preco;
+	
+	public Doce(int codigo, String nome, float preco) throws Exception {
+		this.setCodigo(codigo);
+		this.setNome(nome);
+		this.setPreco(preco);
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) throws Exception {
+		if (codigo <= 0) {
+			throw new Exception("Código inválido");
+		}
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) throws Exception {
+		if (nome.isEmpty()) {
+			throw new Exception("Nome inválido");
+		}
+		this.nome = nome;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) throws Exception {
+		if (preco < 0) {
+			throw new Exception("Preço inválido");
+		}
+		this.preco = preco;
+	}
+}
