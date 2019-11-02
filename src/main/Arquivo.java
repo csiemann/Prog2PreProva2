7813868748dd4ccf4f3f44e8f7e35bafde4d0368
Caetano Siemann <caetanosiemann@gmail.com>
2019-11-01 21:58:16 -0300

    Add Arquivo.java

---
+++ src/main/Arquivo.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Arquivo {
+
+	Arquivo() {
+	}
+
+	public void salvar(String diretorio, String nome, Cliente cliente) {
+
+	}
+
+	public Cliente ler(String diretorio, String nome) {
+		return null;
+	}
+
+	public ArrayList<Cliente> lerLista(String diretorio, String nome) {
+		return null;
+	}
+
+	public String listarArquivos(String diretorio) {
+		return null;
+	}
+}
